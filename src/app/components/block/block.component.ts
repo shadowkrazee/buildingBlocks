03ff81e9d4f0e58de68044647f4cfd4cbe48f5b1
Johnathan <epicshadowkrazee@gmail.com>
2018-06-27 15:14:54 -0400

    changed animation engine from angular to css-based

--- src/app/components/block/block.component.ts
+++ src/app/components/block/block.component.ts
@@ -1,35 +1,34 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { state } from '@angular/animations';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-block',
   templateUrl: './block.component.html',
-  styleUrls: ['./block.component.css'],
-  animations: [
-    trigger('blockState', [
-      state(
-        'inactive',
-        style({
-          backgroundColor: 'black'
-        })
-      ),
-      state(
-        'active',
-        style({
-          backgroundColor: 'red'
-        })
-      ),
-      transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('1000ms ease-out'))
-    ])
-  ]
+  styleUrls: ['./block.component.css']
+  // animations: [
+  //   trigger('blockState', [
+  //     state(
+  //       'inactive',
+  //       style({
+  //         backgroundColor: 'black'
+  //       })
+  //     ),
+  //     state(
+  //       'active',
+  //       style({
+  //         backgroundColor: 'red'
+  //       })
+  //     ),
+  //     transition('inactive => active', animate('100ms ease-in')),
+  //     transition('active => inactive', animate('1000ms ease-out'))
+  //   ])
+  // ]
 })
 export class BlockComponent implements OnInit, OnDestroy {
-  bgColor: number;
   bgColorString: string;
-  hovColor: number;
-  hovColorString: string;
+  defBG: boolean;
+  commonStyleString: string;
   safeStyle: SafeStyle;
   interval: any;
   public state: string;
@@ -38,12 +37,17 @@ export class BlockComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.interval = setInterval(() => {
-      this.toggleState();
-    }, Math.floor(1000 + Math.random() * 9000));
+    this.commonStyleString =
+      'height:2vw;width:2vw;margin:1px;border-radius:5px;transition:background-color 2s ease;';
+    this.bgColorString = 'hsl(0,0%,0%);';
+    this.defBG = false;
+    this.toggleBG();
+    // this.interval = setInterval(() => {
+    //   this.toggleState();
+    // }, Math.floor(1000 + Math.random() * 9000));
   }
   ngOnDestroy() {
-    clearInterval(this.interval);
+    // clearInterval(this.interval);
   }
   toggleState() {
     this.state = this.state === 'active' ? 'inactive' : 'active';
@@ -54,12 +58,26 @@ export class BlockComponent implements OnInit, OnDestroy {
   //   this.hovColor = this.getComplimentaryColor(this.bgColor);
   //   this.makeStrings('notFirst');
   // }
-  randomColor() {
+  randomColorString() {
     let hue = Math.floor(Math.random() * 359);
-    let color = 'hsl(' + hue + ',100%,50%)';
+    let saturation = Math.floor(50 + Math.random() * 50);
+    let color = 'hsl(' + hue + ',' + saturation + '%,50%)';
     return color;
   }
-
+  toggleBG() {
+    let styleString = '';
+    if (!this.defBG) {
+      styleString =
+        this.commonStyleString + 'background-color:' + this.bgColorString;
+      this.defBG = true;
+    } else {
+      styleString =
+        this.commonStyleString + 'background-color:' + this.randomColorString();
+      this.defBG = false;
+    }
+    // console.log('bg changed to hov');
+    this.safeStyle = this.sanitizer.bypassSecurityTrustStyle(styleString);
+  }
   // getComplimentaryColor(color: number) {
   //   let compliment = color - 360;
   //   if (compliment < 0) {
